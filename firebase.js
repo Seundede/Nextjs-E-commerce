3b097770135244972ef11d1da76091990cd2d7be
Seundede <omadoyedede0@gmail.com>
2022-02-08 09:49:37 +0100

    Deleted yarn.lock

--- firebase.js
+++
@@ -1,9 +0,0 @@
-import firebase from "firebase";
-const firebaseConfig = {
-  apiKey: "AIzaSyDnXQCM79HZTGXW8kqxZRihCEC5UNxPY-o",
-  authDomain: "fir-bd446.firebaseapp.com",
-  projectId: "fir-bd446",
-  storageBucket: "fir-bd446.appspot.com",
-  messagingSenderId: "774042160176",
-  appId: "1:774042160176:web:e45583a652699b530df1f5",
-};
