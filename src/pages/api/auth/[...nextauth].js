341a115e8cbc1ef8a670a230f997b0d18e6e47e9
Seundede <omadoyedede0@gmail.com>
2022-02-08 11:25:19 +0100

    auth update

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -7,6 +7,7 @@ export default NextAuth({
     GoogleProvider({
       clientId: process.env.GOOGLE_ID,
       clientSecret: process.env.GOOGLE_SECRET,
-    })
+    }),
   ],
+  secret: "PLACE-HERE-ANY-STRING",
 });
